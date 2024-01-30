31739a20b1170413a8aaa8ddba2e3f76b2fb680c
cedric karungu <ckarungu921@gmail.com>
2024-01-30 08:24:45 +0200

    inline style

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ flexDirection: 'column' }}>
+			<View style={{ backgroundColor: 'green', padding: 4, margin: 4 }}>
+				<Text>Text One</Text>
+			</View>
+			<Text style={{ fontFamily: 'Cochin' }}>Text Two</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		backgroundColor: '#fff',
+// 		alignItems: 'center',
+// 		justifyContent: 'center',
+// 	},
+// });
