004f30f928abc2c14760d7059b0cbfa4808994f1
cedric karungu <ckarungu921@gmail.com>
2024-01-30 08:38:49 +0200

    StyleSheet

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={{ flexDirection: 'column' }}>
-			<View style={{ backgroundColor: 'green', padding: 4, margin: 4 }}>
-				<Text>Text One</Text>
+		<SafeAreaView>
+			<View style={styles.wrapper}>
+				<View style={styles.textBlock}>
+					<Text>Text One</Text>
+					<Text>Text One</Text>
+				</View>
+				<View style={styles.textBlock}>
+					<Text>Text Two</Text>
+					<Text>Text Two</Text>
+					<Text>Text Two</Text>
+					<Text>Text Two</Text>
+				</View>
+				<Text style={styles.textTwo}>Text Two</Text>
+				<StatusBar style='auto' />
 			</View>
-			<Text style={{ fontFamily: 'Cochin' }}>Text Two</Text>
-			<StatusBar style='auto' />
-		</View>
+		</SafeAreaView>
 	);
 }
 
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: '#fff',
-// 		alignItems: 'center',
-// 		justifyContent: 'center',
-// 	},
-// });
+const styles = StyleSheet.create({
+	wrapper: { flexDirection: 'column' },
+	textBlock: {
+		backgroundColor: 'green',
+		padding: 8,
+		margin: 4,
+		borderRadius: 5,
+	},
+	textTwo: {
+		fontSize: 20,
+		textAlign: 'center',
+		fontFamily: 'Cochin',
+		fontWeight: '700',
+		padding: 8,
+	},
+});
